3c26bb68e2b5c7c42a46c7fbae168236bef5d3a9
brain-ambani <brainambani1@gmail.com>
2024-07-11 07:44:14 +0300

    updated the layout and responsiveness for the job listing

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,20 @@ export default async function Home() {
   });
 
   return (
-    <main>
-      {jobs.map((job) => (
-        <JobListItem key={job.id} job={job} />
-      ))}
+    <main className="max-w-5xl m-auto px-3 my-10 space-y-10">
+      <div className="space-y-5 text-center">
+        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Developer jobs
+        </h1>
+        <p className="text-muted-foreground">Find your dream job</p>
+      </div>
+      <section>
+        <div className="space-y-4">
+          {jobs.map((job) => (
+            <JobListItem key={job.id} job={job} />
+          ))}
+        </div>
+      </section>
     </main>
   );
 }
