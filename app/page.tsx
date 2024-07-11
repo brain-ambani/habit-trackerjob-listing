afd9d31447a172bac56f72a98b351d00896bdc08
brain-ambani <brainambani1@gmail.com>
2024-07-11 07:35:26 +0300

    add the job list items

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import { Button } from "@/components/ui/button";
+import JobListItem from "@/components/JobListItem";
+import prisma from "@/lib/prisma";
 
-export default function Home() {
+export default async function Home() {
+  const jobs = await prisma.job.findMany({
+    where: { approved: true },
+    orderBy: { createdAt: "desc" },
+  });
+
   return (
     <main>
-      <Button>Click me</Button>
+      {jobs.map((job) => (
+        <JobListItem key={job.id} job={job} />
+      ))}
     </main>
   );
 }
