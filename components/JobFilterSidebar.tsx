50ce5e684eeb9e45fb8135490a936d0696a797cf
brain-ambani <brainambani1@gmail.com>
2024-07-12 07:25:47 +0300

    added the job type filter

--- components/JobFilterSidebar.tsx
+++ components/JobFilterSidebar.tsx
@@ -1,10 +1,14 @@
+import { JobTypes } from "@/lib/job-types";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import Select from "./ui/select";
 import prisma from "@/lib/prisma";
+import { Button } from "./ui/button";
 
 async function filterJobs(formData: FormData) {
   "use server";
+
+  console.log(formData.get("q") as string);
 }
 
 export default async function JobFilterSidebar() {
@@ -25,10 +29,26 @@ export default async function JobFilterSidebar() {
     >
       <form action={filterJobs}>
         <div className="space-y-4">
+          {/*Search bar for job titles, company, locations and type  */}
           <div className="flex flex-col ,gap-2">
             <Label htmlFor="q">Search</Label>
             <Input id="q" name="q" placeholder="Title, company, etc" />
           </div>
+
+          {/* Job types */}
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="type">Type</Label>
+            <Select id="type" name="type" defaultValue="">
+              <option value="">All locations</option>
+              {JobTypes.map((type) => (
+                <option key={type} value={type}>
+                  {type}
+                </option>
+              ))}
+            </Select>
+          </div>
+
+          {/* locations */}
           <div className="flex flex-col gap-2">
             <Label htmlFor="location">Location</Label>
             <Select id="location" name="location" defaultValue="">
@@ -40,6 +60,18 @@ export default async function JobFilterSidebar() {
               ))}
             </Select>
           </div>
+          <div className="flex items-center gap-2">
+            <input
+              id="remote"
+              type="checkbox"
+              name="remote"
+              className="sclae-125 accent-black"
+            />
+            <Label htmlFor="remote">Remote</Label>
+          </div>
+          <Button type="submit" className="w-full">
+            Filter jobs
+          </Button>
         </div>
       </form>
     </aside>
---
+++ lib/job-types.ts
@@ -0,0 +1,10 @@
+export const JobTypes = [
+  "Full-time",
+  "Part-time",
+  "Contract",
+  "Temporary",
+  "Internship",
+  "Volunteer",
+];
+
+export const locationTypes = ["Remote", "Onsite", "Hybrid"];
